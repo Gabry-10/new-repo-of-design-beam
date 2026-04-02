bc9e653279ee7f8b773ba90ce8f7fa9b49972f60
mohamed walid <M.walid2376@nu.edu.eg>
2026-04-02 17:23:20 +0200

    code of read

---
+++ midtermexam.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df = pd.read_excel("231000476.csv")
+
+Beamid = df["Beam_ID"][0]
+Span = df["Span_m"][0]
+Udl = df["UDL_kN_per_m"][0]
+e = df["E_MPa"][0]
+i =df["I_mm4"][0]
+z= df["Z_mm3"][0]
+fy= df["fy_MPa"][0]
+
+
+
+df.to_csv("result.csv",index=False)
